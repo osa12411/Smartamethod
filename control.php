d874d61c06dd0efc0e68a5b381e191db0a4f047a
osa12411 <osamahalqazlan000@gmail.com>
2024-07-08 11:51:54 +0300

    Update control.php

--- control.php
+++ control.php
@@ -7,7 +7,6 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['direction'])) {
     $direction = $conn->real_escape_string($_POST['direction']);
         $sql = "INSERT INTO robot_movements (direction) VALUES ('$direction')";
-
     if ($conn->query($sql) === TRUE) {
         echo "success";
     } else {
