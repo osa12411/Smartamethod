844726e7076181acf0a64c185a0e6531332284d0
osa12411 <osamahalqazlan000@gmail.com>
2024-07-08 11:50:51 +0300

    first task

---
+++ control.php
@@ -0,0 +1,19 @@
+<?php
+$conn = new mysqli("localhost", "root", "root", "robot_control5");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['direction'])) {
+    $direction = $conn->real_escape_string($_POST['direction']);
+        $sql = "INSERT INTO robot_movements (direction) VALUES ('$direction')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+
+$conn->close();
+?>
